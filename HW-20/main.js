9bbd4c4dbc739a080e17257d8934b7f5878d6488
Sergey Stasyuk <kerrios1986@gmail.com>
2021-04-02 16:54:19 +0300

    HW-20. Done

---
+++ HW-20/main.js
@@ -0,0 +1,26 @@
+document.write(' **************<br>');
+
+a = 10;
+document.write('<br> При <b>а</b> = ', a);
+
+b = 4;
+document.write(', <b>b</b> = ', b);
+
+document.write(' <br> <br> Результат: <br>')
+document.write('<br>--------------------<br> <br>')
+
+res = a + b;
+document.write(' a + b = ', res)
+
+res = a - b;
+document.write('; a - b = ', res)
+
+res = a * b;
+document.write('; a * b = ', res)
+
+res = a / b;
+document.write('; a / b = ', res)
+
+document.write('<br> <br> -------------------- <br> <br>')
+document.write('<br> ************** ')
+
